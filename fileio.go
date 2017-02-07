b3d30c7859136aa4f60ea7d658a2094f99f24b1a
Raed Atoui <ratoui@hugeinc.com>
2017-02-07 01:57:02 -0500

    first commit

---
+++ fileio.go
@@ -0,0 +1,21 @@
+package glutils
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func readFile(file string) ([]byte, error) {
+	reader, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+
+	data, err := ioutil.ReadAll(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return data, err
+}
